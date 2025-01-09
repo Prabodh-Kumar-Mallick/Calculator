3119edb0ffd83861bf175005b05f9929e7aa7b0a
Akash <mallickprabodhkumar@gmail.com>
2025-01-09 23:30:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ function Display(num){
 
 function CalculateResult(){
     let v = document.getElementById('inp').value
+    try{
     document.getElementById('inp').value = eval(v)
 }
 
+catch(error)
+{
+    document.getElementById('inp').value ="Invalid input";
+}
+}
 function Delete(){
     let v = document.getElementById("inp").value
     v = v.slice(0,-1)
